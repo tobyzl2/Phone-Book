7742386fe3316b6e05b62abbd34aec983068c6fd
tobyzl2 <tobyzl2@illinois.edu>
2019-06-20 16:24:12 -0700

    Simple test server implemented

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Testing");
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}...`);
+});
